5e27540cc475d896abe7d55eb22f70d24891caf0
webNie <3048615550@qq.com>
2019-08-02 16:17:32 +0800

    20190802NYH

--- project-msny/msny/src/App.js
+++ project-msny/msny/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          河北plus合作团队，敬请期待
         </p>
         <a
           className="App-link"
