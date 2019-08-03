686de4d6dc808c9b885b60721c084d4c4d979284
wenbo <1050794513@qq.com>
2019-08-03 16:25:44 +0800

    miaoshu

---
+++ project-msny/msny/src/components/index.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Html extends Component {
+    render() {
+        return (
+            <div>
+                <p>ofwefowoefhowef</p>
+            </div>
+        )
+    }
+}
---
+++ project-msny/msny/src/components/login/index.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Login extends Component {
+    render() {
+        return (
+            <div>
+                11111111111111
+            </div>
+        )
+    }
+}
